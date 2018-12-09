a3ff55df1d37fbeddf0ac85a1afc0c84234dc264
Fraser Henshaw <fraser.Henshaw@.onyxnetworks.co.uk>
2018-12-09 13:23:37 +0000

    First commit

--- deployment_tool/settings.py
+++ deployment_tool/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/2.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
-
+# Import sensitive data
+from .secrets.py import *
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -18,9 +19,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '*6hg3*+%lpr59w^am_v(%x+7q))2wnop*uw2eg7$66wudrmpua'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
