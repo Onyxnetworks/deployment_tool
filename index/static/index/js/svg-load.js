39e8b38e1b452119733ae052f4b9e6b223ca5972
Fraser Henshaw <Fraser.Henshaw@onyxnetworks.co.uk>
2018-12-10 18:28:40 +0000

    updated tasks.py

--- index/static/index/js/svg-load.js
+++ index/static/index/js/svg-load.js
@@ -1,5 +1,5 @@
 loading_spinner = new XMLHttpRequest();
-loading_spinner.open("GET","% static 'index/svg/spinner.svg' %}",false);
+loading_spinner.open("GET","static/index/svg/spinner.svg",false);
 // Following line is just to be on the safe side;
 // not needed if your server delivers SVG with correct MIME type
 loading_spinner.overrideMimeType("image/svg+xml");
