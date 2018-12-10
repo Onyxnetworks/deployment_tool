6ff303c5b1553feb61ec4e41f224ca52cc5d58d6
Fraser Henshaw <Fraser.Henshaw@onyxnetworks.co.uk>
2018-12-10 21:41:46 +0000

    updated tasks.py

--- index/urls.py
+++ index/urls.py
@@ -2,8 +2,9 @@ from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
-    url(r'^index/', views.index, name='Index'),
-    url(r'login/', views.login, name='Login'),
+    url(r'^index/', views.index, name='index'),
+    url(r'login/', views.login, name='login'),
     url(r'^get_task_info/', views.get_task_info, name='get_task_info'),
+    url(r'^logout/', views.login, name='logout'),
 ]
 
--- index/views.py
+++ index/views.py
@@ -1,8 +1,9 @@
 import json
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from celery.result import AsyncResult
 
+from aci_deployment.scripts.baseline import APIC_LOGIN
 
 # Function to get task state and results to be used by ajax.
 def get_task_info(request):
@@ -13,13 +14,53 @@ def get_task_info(request):
             'state': task.state,
             'result': task.result,
         }
-        print(type(data))
+
         return HttpResponse(json.dumps(data), content_type='application/json')
+
     else:
         return HttpResponse('No job id given.')
 
+
 def login(request):
+    if request.method == 'POST':
+        if 'username' and 'password' in request.POST:
+            username = request.POST['username']
+            password = request.POST['password']
+            environment = request.POST['environment']
+
+            if environment == 'Production':
+                request.session['prod_username'] = username
+                request.session['prod_password'] = password
+
+
+            elif environment == 'Pre-Production':
+                request.session['ppe_username'] = username
+                request.session['ppe_password'] = password
+
+            elif environment == 'Lab':
+                request.session['lab_username'] = username
+                request.session['lab_password'] = password
+
+            apic_cookie = APIC_LOGIN(base_url, username, password)
+
+            if apic_cookie:
+                request.session['APIC_COOKIE'] = apic_cookie
+                return redirect('Endpoint_Search')
+
+            else:
+                message = 'Unable to authenticate, please check credentials.'
+                return HttpResponse(json.dumps({'message': message}),
+                                    content_type='application/json')
+
+
     return render(request, 'index/login.html')
+
 
 def index(request):
     return render(request, 'index/home.html')
+
+
+def logout(request):
+    # Delete session data containing user details
+    request.session.flush()
+    return redirect(index)
