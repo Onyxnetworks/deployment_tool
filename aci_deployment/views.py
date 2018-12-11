c93f6c80ebdd30f9d4d9344d68402bdb8ac44631
Fraser.Henshaw <Fraser.Henshaw@onyxnetworks.co.uk>
2018-12-11 14:07:07 +0000

    Updates

--- aci_deployment/views.py
+++ aci_deployment/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 from .tasks import *
-from aci_deployment.scripts.secrets import *
 from index.scripts.baseline import get_base_url
 
 
