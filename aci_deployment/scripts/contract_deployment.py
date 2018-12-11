6ba866e82df1ff84f5895a64d9e4a8988f4b039c
Fraser.Henshaw <Fraser.Henshaw@onyxnetworks.co.uk>
2018-12-11 14:36:32 +0000

    Updates

--- aci_deployment/scripts/contract_deployment.py
+++ aci_deployment/scripts/contract_deployment.py
@@ -315,47 +315,6 @@ def GET_ENDPOINT_EXTERNAL(BASE_URL, APIC_COOKIE, HEADERS):
     except:
         exit('Failed to get External Endpoint Data.')
 
-
-def GET_ENDPOINTS(BASE_URL, APIC_USERNAME, APIC_PASSWORD):
-    ENDPOINT_INTERNAL_LIST = []
-    ENDPOINT_EXTERNAL_LIST = []
-    LOCATION = 'LAB'
-    HEADERS = {'content-type': 'application/json'}
-    # Login to fabric
-    APIC_COOKIE = APIC_LOGIN(BASE_URL, APIC_USERNAME, APIC_PASSWORD)
-    # Get External Endpoints
-    ENDPOINT_EXTERNAL_RESPONSE = GET_ENDPOINT_EXTERNAL(BASE_URL, APIC_COOKIE, HEADERS)
-    # Get Internal Endpoint data.
-    ENDPOINT_INTERNAL_RESPONSE = GET_ENDPOINT_INTERNAL(BASE_URL, APIC_COOKIE, HEADERS)
-    # Loop over endpoints and build endpoint lists.
-    for i in ENDPOINT_EXTERNAL_RESPONSE['imdata']:
-        IMPORT = ''
-        EXPORT = ''
-        SECURITY = ''
-        if 'import-rtctrl' in i['l3extSubnet']['attributes']['scope']:
-            IMPORT = 'I '
-        if 'export-rtctrl' in i['l3extSubnet']['attributes']['scope']:
-            EXPORT = 'E '
-        if 'import-security' in i['l3extSubnet']['attributes']['scope']:
-            SECURITY = 'S '
-
-        SCOPE = IMPORT + EXPORT + SECURITY
-
-        ENDPOINT_EXTERNAL_LIST.append(
-            {'Location': LOCATION, 'Tenant': i['l3extSubnet']['attributes']['dn'].split('/')[1][3:],
-             'AppProfile': i['l3extSubnet']['attributes']['dn'].split('/')[2][4:],
-             'EPG': i['l3extSubnet']['attributes']['dn'].split('/')[3][6:],
-             'Subnet': i['l3extSubnet']['attributes']['ip'], 'Scope': SCOPE, 'Locality': 'External'})
-
-    for i in ENDPOINT_INTERNAL_RESPONSE['imdata']:
-        ENDPOINT_EXTERNAL_LIST.append({'Location': LOCATION, 'Tenant': i['fvCEp']['attributes']['dn'].split('/')[1][3:],
-                                       'AppProfile': i['fvCEp']['attributes']['dn'].split('/')[2][3:],
-                                       'EPG': i['fvCEp']['attributes']['dn'].split('/')[3][4:],
-                                       'Subnet': i['fvCEp']['attributes']['ip'], 'Scope': '', 'Locality': 'Internal'})
-
-    return ENDPOINT_EXTERNAL_LIST
-
-
 def CONTRACT_DEPLOYMENT_EXCEL_FORMAT_VALIDATION(RULE_LIST):
     OUTPUT_LOG = []
     DISPLAY_LIST = []
