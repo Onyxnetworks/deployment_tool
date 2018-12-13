20cae82b5f8d4bac3df460e11c55d17027a6d759
Fraser.Henshaw <Fraser.Henshaw@onyxnetworks.co.uk>
2018-12-13 14:23:12 +0000

    Updates

--- f5_deployment/views.py
+++ f5_deployment/views.py
@@ -6,5 +6,38 @@ from index.scripts.baseline import get_base_url
 
 
 def vs_deployment(request):
+    # Present file upload to screen and give options to user
+    if request.method == 'POST' and 'file' in request.FILES:
+        file = request.FILES['file']
+        location = request.POST['location']
+
+        environment = request.session.get('environment')
+        if environment == 'Production':
+            username = request.session.get('prod_username')
+            password = request.session.get('prod_password')
+
+
+        elif environment == 'Pre-Production':
+            # Need to put in an error as PPE search wont work!
+            username = request.session.get('ppe_username')
+            password = request.session.get('ppe_password')
+
+        elif environment == 'Lab':
+            username = request.session.get('lab_username')
+            password = request.session.get('lab_password')
+
+        # Get base url to use
+        base_urls = get_base_url(environment)
+        url_dict = base_urls['F5']
+
+        # Open workbook and build jason data structure.
+        rule_list = vs_deployment_excel_open_workbook(file, location)
+
+        # Validate Request names and format
+        task = vs_deployment_validation.delay(rule_list, location, url_dict, username, password)
+
+        # Return task id.
+        return HttpResponse(json.dumps({'task_id': task.id, 'rule_list': rule_list, 'location': location}),
+                            content_type='application/json')
     content = {}
     return render(request, 'f5_deployment/vs_deployment.html', content)
