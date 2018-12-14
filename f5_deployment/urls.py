2f6ee263916b1e4a8c5584873f4160fb019f2fc9
Fraser Henshaw <Fraser.Henshaw@onyxnetworks.co.uk>
2018-12-14 21:47:24 +0000

    updated tasks.py

--- f5_deployment/urls.py
+++ f5_deployment/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^vs_deployment/$', views.vs_deployment, name='vs_deployment'),
+    url(r'^vs_deployment_push/$', views.vs_deployment_push, name='vs_deployment_push'),
 ]
