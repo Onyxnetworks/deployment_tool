7b5872b429e54ba9c1ac1b4a755ee48747476cf3
Fraser Henshaw <Fraser.Henshaw@onyxnetworks.co.uk>
2018-12-18 23:17:13 +0000

    updated tasks.py

--- f5_deployment/scripts/generic_search.py
+++ f5_deployment/scripts/generic_search.py
@@ -105,7 +105,7 @@ def virtual_server_dashboard(url_list, username, password):
 
 
             except:
-                results.append({'location': location, 'vs_name': vs_name, 'vs_state': vs_state, 'vs_pool': {'pool_name': 'none', 'pool_state': 'unknown'}})
+                results.append({'location': location, 'vs_name': vs_name, 'vs_state': vs_state, 'vs_ip': 'none', 'vs_pool': {'pool_name': 'none', 'pool_state': 'unknown'}})
 
         return results
 
