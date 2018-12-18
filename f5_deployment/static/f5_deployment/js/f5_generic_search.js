6b46a6334c1faf8223b83433850cc15d0e41f104
Fraser Henshaw <Fraser.Henshaw@onyxnetworks.co.uk>
2018-12-18 21:56:14 +0000

    updated tasks.py

--- f5_deployment/static/f5_deployment/js/f5_generic_search.js
+++ f5_deployment/static/f5_deployment/js/f5_generic_search.js
@@ -31,18 +31,18 @@ function get_task_info(task_id) {
             }
             else if (data.state == 'SUCCESS') {
                 console.log("success");
-                console.log(data.result);
 //                <!-- Clear Old table Data -->
-                //document.getElementById("result_table").innerHTML = "";
-                //document.getElementById("tablediv").style.visibility = "visible";
-                //var results = data.result;
-                //for (i = 0, len = results.length, text = ""; i < len; i++) {
-                    //location = results[i].location;
-                    //vs_name = results[i].vs_name;
-                    //AppProfile = results[i].AppProfile;
-                    //vs_status = results[i].vs_status;
-                    //pool_name = results[i].pool_name;
-                    //pool_status = results[i].pool_status;
+                document.getElementById("result_table").innerHTML = "";
+                document.getElementById("tablediv").style.visibility = "visible";
+                var results = data.result;
+                for (i = 0, len = results.length, text = ""; i < len; i++) {
+                    f5_location = results[i].location;
+                    vs_name = results[i].vs_name;
+                    vs_status = results[i].vs_status;
+                    pool_name = results[i].pool_name;
+                    pool_status = results[i].pool_status;
+                    console.log(f5_location, vs_name, vs_status, pool_name, pool_status );
+
                     //if (vs_status.includes('available')) {
                     //    var vs_status_img = "<img src='{% static 'f5_deployment/img/status_circle_green.png' %}' class='img-responsive' alt='Responsive image'>"
                     //}
@@ -72,7 +72,8 @@ function get_task_info(task_id) {
                         //td.innerHTML = items;
                         //document.body.appendChild(td);
                         //document.getElementById(TABLE_TR).appendChild(td);
-                   // });}
+                    //});
+            }
 
             }
 
