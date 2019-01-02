b6ba05a65e6cc9ed91d3604533b43d5dbacb197d
Fraser Henshaw <Fraser.Henshaw@onyxnetworks.co.uk>
2019-01-02 11:15:33 +0000

    updated tasks.py

--- index/views.py
+++ index/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse
 from celery.result import AsyncResult
 
 from aci_deployment.scripts.baseline import APIC_LOGIN
+from f5_deployment.scripts.baseline import bigip_login
 from index.scripts.baseline import get_base_url
 
 # Function to get task state and results to be used by ajax.
@@ -35,7 +36,6 @@ def login(request):
                 request.session['prod_username'] = username
                 request.session['prod_password'] = password
 
-
             elif environment == 'Pre-Production':
                 request.session['ppe_username'] = username
                 request.session['ppe_password'] = password
@@ -47,20 +47,34 @@ def login(request):
             # Get base url to use for authentication and scripts and try to login to UKDC1 APIC
             base_urls = get_base_url(environment)
 
-            # Get a value from the dictionary to use for login URL.
-            base_url = next(iter(base_urls['ACI'].values()))
-
             # Attempt to authenticate user
-            apic_cookie = APIC_LOGIN(base_url, username, password)
+            if environment == 'Production' or 'Lab':
+                # Get a value from the dictionary to use for login URL.
+                base_url = next(iter(base_urls['ACI'].values()))
+                apic_cookie = APIC_LOGIN(base_url, username, password)
 
-            if apic_cookie:
-                request.session['APIC_COOKIE'] = apic_cookie
-                return redirect(index)
+                if apic_cookie:
+                    request.session['APIC_COOKIE'] = apic_cookie
+                    return redirect(index)
 
-            else:
-                content = {'error': True, 'message': 'Unable to authenticate, please check credentials.'}
-                redirect(request.path_info)
+                else:
+                    content = {'error': True, 'message': 'Unable to authenticate, please check credentials.'}
+                    redirect(request.path_info)
 
+            if environment == 'Pre-Production':
+                # Get a value from the dictionary to use for login URL.
+                base_url = next(iter(base_urls['F5'].values()))
+                base_url = next(iter(base_url.values()))
+
+                auth_token = bigip_login(base_url, username, password)
+
+                if auth_token:
+                    request.session['APIC_COOKIE'] = auth_token
+                    return redirect(index)
+
+                else:
+                    content = {'error': True, 'message': 'Unable to authenticate, please check credentials.'}
+                    redirect(request.path_info)
 
     return render(request, 'index/login.html', content)
 
