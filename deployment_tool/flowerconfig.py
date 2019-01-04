40c413612c9d4e6a4aa036aa95db804e31b7759c
Fraser Henshaw <Fraser.Henshaw@onyxnetworks.co.uk>
2019-01-04 14:46:42 +0000

    updated tasks.py

--- deployment_tool/flowerconfig.py
+++ deployment_tool/flowerconfig.py
@@ -1,12 +1,6 @@
 from flower.utils.template import humanize
 
 def format_task(task):
-    # Change the task.args string to a tuple then a list.
-    task.args = list(eval(task.args))
-    # Remove password field from list (password is always last item in list).
-    del task.args[-1]
-    # Change it back to a tuple then a string.
-    task.args = str(tuple(task.args))
     task.args = humanize(task.args, length=20)
     task.result = humanize(task.result, length=20)
     return task
