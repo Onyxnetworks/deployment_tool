128b2eacfeb836c9cb768dcb74a0480de3944915
Fraser Henshaw <Fraser.Henshaw@onyxnetworks.co.uk>
2019-01-04 13:25:52 +0000

    updated tasks.py

--- deployment_tool/flowerconfig.py
+++ deployment_tool/flowerconfig.py
@@ -2,7 +2,7 @@ from flower.utils.template import humanize
 
 def format_task(task):
     task.args = humanize(task.args, length=10)
-    print(task.kwargs)
+    print(task.args)
     #task.kwargs.pop('password')
     #task.result = humanize(task.result, length=20)
     return task
