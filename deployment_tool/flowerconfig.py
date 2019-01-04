7d3d4bd4cc8d6bfedd0d04a76b2add95ba0e2a45
Fraser Henshaw <Fraser.Henshaw@onyxnetworks.co.uk>
2019-01-04 14:19:38 +0000

    updated tasks.py

--- deployment_tool/flowerconfig.py
+++ deployment_tool/flowerconfig.py
@@ -1,16 +1,16 @@
 from flower.utils.template import humanize
 
 def format_task(task):
-    print(type(task))
-    task.args = list(task.args)
-
-    print(type(task.args))
+    # Change the task.args string to a tuple then a list.
+    task.args = list(eval(task.args))
+    # Remove password field from list (password is always last item in list).
+    del task.args[-1]
+    # Change it back to a tuple then a string.
+    task.args = str(tuple(task.args))
+    task.args = humanize(task.args)
     print(task.args)
-    print(task.kwargs)
-    #task.args = humanize(task.args)
-    #ask.kwargs.pop('password')
-    #task.result = humanize(task.result, length=20)
-    task.args = str(task.args)
+    #task.kwargs.pop('password')
+    task.result = humanize(task.result, length=20)
     return task
 
 basic_auth = ['home:mote74']
