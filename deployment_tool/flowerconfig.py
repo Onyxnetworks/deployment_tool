10c89395fac1ab975e47a2f44c77fad8e2f539f7
Fraser Henshaw <Fraser.Henshaw@onyxnetworks.co.uk>
2019-01-04 13:37:01 +0000

    updated tasks.py

--- deployment_tool/flowerconfig.py
+++ deployment_tool/flowerconfig.py
@@ -2,7 +2,9 @@ from flower.utils.template import humanize
 
 def format_task(task):
     args_list = task.args.split()
+    print(args_list)
     args_no_pass = args_list.remove(args_list[-1])
+    print(args_no_pass)
     task.args = str(args_no_pass)
     task.args = humanize(task.args, length=10)
     print(type(task.args))
