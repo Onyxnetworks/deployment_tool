b1c368702b90f4d7b1a7a8e2cca3621f91fbbf7b
Fraser Henshaw <Fraser.Henshaw@onyxnetworks.co.uk>
2019-01-04 17:30:44 +0000

    updated tasks.py

--- deployment_tool/flowerconfig.py
+++ deployment_tool/flowerconfig.py
@@ -1,9 +1,10 @@
 from flower.utils.template import humanize
+from deployment_tool.secrets import *
 
 def format_task(task):
     task.args = humanize(task.args, length=20)
     task.result = humanize(task.result, length=20)
     return task
 
-basic_auth = ['home:mote74']
+basic_auth = flower_basic_auth
 port = '8443'
