a683d5c003301afc71f606283ba677b13326a76c
Fraser Henshaw <Fraser.Henshaw@onyxnetworks.co.uk>
2019-01-12 15:26:30 +0000

    updated tasks.py

--- f5_deployment/static/f5_deployment/js/f5_generic_search.js
+++ f5_deployment/static/f5_deployment/js/f5_generic_search.js
@@ -20,7 +20,7 @@ function node_control_off() {
     var checkedItems = $('#nodes_table input[type="checkbox"]:checked').each(function() {
 
         // Do something with the row
-        console.log($(this).parent('tr').data('url'));
+        console.log($(this).parent('tr').id.value());
     });
     if (!checkedItems.length) {
         console.log('Nothing Checked')
