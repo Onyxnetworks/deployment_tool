caafd65c4890df280db97497ce79530b72c5c4f3
Fraser Henshaw <Fraser.Henshaw@onyxnetworks.co.uk>
2019-01-12 23:07:39 +0000

    updated tasks.py

--- f5_deployment/static/f5_deployment/js/f5_generic_search.js
+++ f5_deployment/static/f5_deployment/js/f5_generic_search.js
@@ -4,16 +4,92 @@ var enable_json = {"state": "user-up", "session": "user-enabled"};
 var disable_json = {"state": "user-up", "session": "user-disabled"};
 
 
+function build_result_table(data) {
+
+    document.getElementById("loader").style.display = "none";
+    document.getElementById("result_table").innerHTML = "";
+    document.getElementById("tablediv").style.visibility = "visible";
+    //document.getElementById("vs_data_body").innerHTML = "";
+    var results = data.result.data;
+    window.search_string = data.result.search.search_string;
+    window.request_type = data.result.search.request_type;
+    for (i = 0, len = results.length, text = ""; i < len; i++) {
+        f5_location = results[i].location;
+        vs_name = results[i].vs_name;
+        vs_ip = results[i].vs_ip;
+        vs_status = results[i].vs_state;
+        vs_admin_state = results[i].vs_admin_state;
+        vs_state_reason = results[i].vs_state_reason;
+        pool_name = results[i].vs_pool.pool_name;
+        pool_status = results[i].vs_pool.pool_state;
+        pool_state_reason = results[i].vs_pool.pool_state_reason;
+        if (vs_admin_state.includes('disabled')){
+            if (vs_status.includes('available')) {
+                var vs_status_img = `<img src='/static/f5_deployment/img/status_circle_black.png' class='img-responsive center-block' alt='vs_available' title=${vs_state_reason}">`
+            }
+            if (vs_status.includes('offline')) {
+                var vs_status_img = `<img src='/static/f5_deployment/img/status_diamond_black.png' class='img-responsive center-block' alt='vs_offline' title="${vs_state_reason}">`
+            }
+            if (vs_status.includes('unknown')) {
+                var vs_status_img = `<img src='/static/f5_deployment/img/status_square_black.png' class='img-responsive center-block' alt='vs_unknown' title="${vs_state_reason}">`
+            }
+        }
+        if (vs_admin_state.includes('enabled')){
+            if (vs_status.includes('available')) {
+                var vs_status_img = `<img src='/static/f5_deployment/img/status_circle_green.png' class='img-responsive center-block' alt='vs_available' title="${vs_state_reason}">`
+            }
+            if (vs_status.includes('offline')) {
+                var vs_status_img = `<img src='/static/f5_deployment/img/status_diamond_red.png' class='img-responsive center-block' alt='vs_offline' title="${vs_state_reason}">`
+            }
+            if (vs_status.includes('unknown')) {
+                var vs_status_img = `<img src='/static/f5_deployment/img/status_square_blue.png' class='img-responsive center-block' alt='vs_unknown' title="${vs_state_reason}">`
+            }
+        }
+        if (pool_name != 'none'){
+            if (pool_status.includes('available')) {
+                var pool_status_img = `<img src='/static/f5_deployment/img/status_circle_green.png' class='img-responsive center-block' alt='pool_available' title="${pool_state_reason}">`
+            }
+            if (pool_status.includes('offline')) {
+                var pool_status_img = `<img src='/static/f5_deployment/img/status_diamond_red.png' class='img-responsive center-block' alt='pool_offline' title="${pool_state_reason}">`
+            }
+            if (pool_status.includes('unknown')) {
+                var pool_status_img = `<img src='/static/f5_deployment/img/status_square_blue.png' class='img-responsive center-block' alt='pool_unknown' title="${pool_state_reason}">`
+            }
+        }
+        if (pool_name == 'none'){
+            var pool_status_img = ''
+        }
+        var tr = document.createElement("TR");
+        var TABLE_TR = 'TABLE_TR_' + i
+        tr.setAttribute("id", TABLE_TR);
+        tr.setAttribute("data-url", `${i}`);
+        if (vs_admin_state.includes('disabled')){
+            tr.setAttribute("class", "muted clickable-row");
+        }
+        else {
+            if (vs_status.includes('available')){
+                tr.setAttribute("class", "success clickable-row");
+            }
+            if (vs_status.includes('offline')){
+                tr.setAttribute("class", "danger clickable-row");
+            }
+            if (vs_status.includes('unknown')){
+                tr.setAttribute("class", "info clickable-row");
+            }
+        }
+        document.getElementById("result_table").appendChild(tr);
+        var search_results = [f5_location, vs_name, vs_ip, vs_status_img, pool_name, pool_status_img]
+        search_results.forEach(function(items) {
+            var td = document.createElement("TD");
+            td.setAttribute("style", "text-align: center; vertical-align: middle;");
+            td.innerHTML = items;
+            document.body.appendChild(td);
+            document.getElementById(TABLE_TR).appendChild(td);
+        });
+    }
 
 
-//function initdatatable() {
-//    table = $('#result_table_header').DataTable({
-//            responsive: true,
-//            "language": {
-//                "search": "Filter records:"
-//            },
-//        } );
-//}
+}
 
 function node_control_on_off(action) {
     // Create empty list
@@ -99,94 +175,8 @@ function get_task_info(task_id) {
                 rslt.html('Searching BigIP...');
             }
             else if (data.state == 'SUCCESS') {
-//                <!-- Clear Old table Data -->
-                if ($.fn.DataTable.isDataTable("#result_table_header")){
-                     $('#result_table_header').DataTable().clear().destroy();
-                     result_table;
-                }
-                document.getElementById("loader").style.display = "none";
-                document.getElementById("result_table").innerHTML = "";
-                document.getElementById("tablediv").style.visibility = "visible";
-                //document.getElementById("vs_data_body").innerHTML = "";
-                var results = data.result.data;
-                window.search_string = data.result.search.search_string;
-                window.request_type = data.result.search.request_type;
-                for (i = 0, len = results.length, text = ""; i < len; i++) {
-                    f5_location = results[i].location;
-                    vs_name = results[i].vs_name;
-                    vs_ip = results[i].vs_ip;
-                    vs_status = results[i].vs_state;
-                    vs_admin_state = results[i].vs_admin_state;
-                    vs_state_reason = results[i].vs_state_reason;
-                    pool_name = results[i].vs_pool.pool_name;
-                    pool_status = results[i].vs_pool.pool_state;
-                    pool_state_reason = results[i].vs_pool.pool_state_reason;
-                    if (vs_admin_state.includes('disabled')){
-                        if (vs_status.includes('available')) {
-                            var vs_status_img = `<img src='/static/f5_deployment/img/status_circle_black.png' class='img-responsive center-block' alt='vs_available' title=${vs_state_reason}">`
-                        }
-                        if (vs_status.includes('offline')) {
-                            var vs_status_img = `<img src='/static/f5_deployment/img/status_diamond_black.png' class='img-responsive center-block' alt='vs_offline' title="${vs_state_reason}">`
-                        }
-                        if (vs_status.includes('unknown')) {
-                            var vs_status_img = `<img src='/static/f5_deployment/img/status_square_black.png' class='img-responsive center-block' alt='vs_unknown' title="${vs_state_reason}">`
-                        }
-                    }
-                    if (vs_admin_state.includes('enabled')){
-                        if (vs_status.includes('available')) {
-                            var vs_status_img = `<img src='/static/f5_deployment/img/status_circle_green.png' class='img-responsive center-block' alt='vs_available' title="${vs_state_reason}">`
-                        }
-                        if (vs_status.includes('offline')) {
-                            var vs_status_img = `<img src='/static/f5_deployment/img/status_diamond_red.png' class='img-responsive center-block' alt='vs_offline' title="${vs_state_reason}">`
-                        }
-                        if (vs_status.includes('unknown')) {
-                            var vs_status_img = `<img src='/static/f5_deployment/img/status_square_blue.png' class='img-responsive center-block' alt='vs_unknown' title="${vs_state_reason}">`
-                        }
-                    }
 
-                    if (pool_name != 'none'){
-                        if (pool_status.includes('available')) {
-                            var pool_status_img = `<img src='/static/f5_deployment/img/status_circle_green.png' class='img-responsive center-block' alt='pool_available' title="${pool_state_reason}">`
-                        }
-                        if (pool_status.includes('offline')) {
-                            var pool_status_img = `<img src='/static/f5_deployment/img/status_diamond_red.png' class='img-responsive center-block' alt='pool_offline' title="${pool_state_reason}">`
-                        }
-                        if (pool_status.includes('unknown')) {
-                            var pool_status_img = `<img src='/static/f5_deployment/img/status_square_blue.png' class='img-responsive center-block' alt='pool_unknown' title="${pool_state_reason}">`
-                        }
-                    }
-                    if (pool_name == 'none'){
-                        var pool_status_img = ''
-                    }
-
-                    var tr = document.createElement("TR");
-                    var TABLE_TR = 'TABLE_TR_' + i
-                    tr.setAttribute("id", TABLE_TR);
-                    tr.setAttribute("data-url", `${i}`);
-                    if (vs_admin_state.includes('disabled')){
-                        tr.setAttribute("class", "muted clickable-row");
-                    }
-                    else {
-                        if (vs_status.includes('available')){
-                            tr.setAttribute("class", "success clickable-row");
-                        }
-                        if (vs_status.includes('offline')){
-                            tr.setAttribute("class", "danger clickable-row");
-                        }
-                        if (vs_status.includes('unknown')){
-                            tr.setAttribute("class", "info clickable-row");
-                        }
-                    }
-                    document.getElementById("result_table").appendChild(tr);
-                    var search_results = [f5_location, vs_name, vs_ip, vs_status_img, pool_name, pool_status_img]
-                    search_results.forEach(function(items) {
-                        var td = document.createElement("TD");
-                        td.setAttribute("style", "text-align: center; vertical-align: middle;");
-                        td.innerHTML = items;
-                        document.body.appendChild(td);
-                        document.getElementById(TABLE_TR).appendChild(td);
-                    });
-                }
+                build_result_table(data);
 
                 //var result_table = $('#result_table_header').DataTable({
                 //    retrieve: true,
