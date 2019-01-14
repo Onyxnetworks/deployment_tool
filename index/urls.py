26f2d7664f65d600f6d2468477d12831f3ff3a67
Fraser Henshaw <Fraser.Henshaw@onyxnetworks.co.uk>
2019-01-14 23:01:56 +0000

    updated tasks.py

--- index/urls.py
+++ index/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     url(r'^index/', views.index, name='index'),
     url(r'^login/', views.admin_login, name='admin_login'),
-    url(r'^$', views.login, name='login'),
+    url(r'^$', views.admin_login, name='admin_login'),
     url(r'^get_task_info/', views.get_task_info, name='get_task_info'),
     url(r'^logout/', views.admin_logout, name='admin_logout'),
 ]
